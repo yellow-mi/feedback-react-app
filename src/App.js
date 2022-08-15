f5dc9ca24b46449ee73fc842b951e19a4db52398
yellow-mi <miriam.lent11@gmail.com>
2022-08-15 08:52:08 +0200

    feat: add feedback functionalities

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import FeedbackData from "./data/feedbackData";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
@@ -18,7 +24,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,26 +2,23 @@ import PropTypes from 'prop-types'
 
 function Button({ children, version, type, isDisabled }) {
   return (
-    <button
-    className={`btn btn-${version}`}
-    isDisabled={isDisabled}
-    type={type}>
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
       {children}
     </button>
   )
 }
 
 Button.defaultProps = {
-  isDisabled: false,
+  version: 'primary',
   type: 'button',
-  version: 'primary'
+  isDisabled: false,
 }
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  isDisabled: PropTypes.bool,
+  version: PropTypes.string,
   type: PropTypes.string,
-  version: PropTypes.string
+  isDisabled: PropTypes.bool,
 }
 
 export default Button
