38f7347944bc8798d3ee656377890a804a22bbd2
yellow-mi <miriam.lent11@gmail.com>
2022-08-16 13:48:00 +0200

    feat: fetch data from the mock server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is the number 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item is the number 2',
-      rating: 6,
-    },
-    {
-      id: 3,
-      text: 'This item is the number 1',
-      rating: 8,
-    },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback data
+  const fetchFeedback = async () => {
+    const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Add feedback
   const addFeedback = (newFeedback) => {
@@ -63,6 +60,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback,
         feedbackEdit,
         feedback,
+        isLoading,
         updateFeedback
       }}
     >
